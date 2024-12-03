a6f51414a471c1e452b7a567d007dd6e2035689d
wick-007 <158535028+wick-007@users.noreply.github.com>
2024-12-03 15:35:00 +0000

    moving on to the next project

--- React-tut/src/components/Header.jsx
+++ React-tut/src/components/Header.jsx
@@ -1,17 +1,20 @@
 import reactLogo from "../assets/react.svg";
 
 export const Header = () => {
-    return (
-       <header className="flex justify-between"> 
-      <img src={reactLogo}  alt="react-logo"/>
-      <nav>
-        <ul className="flex justify-around mr-4 " >
-          <li className="mr-4 ">About</li>
-          <li className="mr-4 ">Contact</li>
-          <li className="mr-4 ">Pricing</li>
-        </ul>
-      </nav>
-      </header>
-    )
-  }
-  
+  return (
+    <div className="flex justify-between bg-blue-950 h-20">
+      <header className="flex align-middle">
+        <img src={reactLogo} alt="react-logo" />
+        <span className="font-bold text-sky-400">REACTFACTS</span>
+        </header>
+        <nav>
+          <ul className="flex justify-around mr-4 ">
+            <li className="mr-4 ">About</li>
+            <li className="mr-4 ">Contact</li>
+            <li className="mr-4 ">Pricing</li>
+          </ul>
+        </nav>
+     
+    </div>
+  );
+};
--- React-tut/src/components/Page.jsx
+++ React-tut/src/components/Page.jsx
@@ -1,8 +1,8 @@
 export const Main = () => {
   return (
     <div>
-      <main className="min-h-screen flex flex-col">
-        <h1>Reasons I am excited to learn about react </h1>
+      <main className=" flex flex-col text-white">
+        <h1 className="text-2xl font-medium">Reasons I am excited to learn about react </h1>
         <ol>
           <li>React is a popular library</li>
           <li>I am more likely to get a job as a frontend developer</li>
