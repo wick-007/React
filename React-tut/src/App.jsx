8b944c00a408dfee71436b8267a16976107a78b8
wick-007 <158535028+wick-007@users.noreply.github.com>
2024-12-04 11:34:40 +0000

    learning props

--- React-tut/src/App.jsx
+++ React-tut/src/App.jsx
@@ -1,21 +1,39 @@
-
+import Contact from "./components/TravelPage/Contact";
 import { TravelHeader } from "./components/TravelPage/TravelHeader";
 
-
-
 function App() {
-
   return (
     <div>
-     <TravelHeader/>
-      
+      <TravelHeader />
+      <div className="flex">
+        <Contact
+          img="../public/dog1.jpg"
+          name="Bruno"
+          phone="+00 6784 345"
+          mail="bruno@mail.com"
+        />
+        <Contact
+          img="../public/dog2.jpg"
+          name="Gimenez"
+          phone="+90 6784 345"
+          mail="gim@mail.com"
+        />
+        <Contact
+          img="../public/dog3.jpg"
+          name="Raul"
+          phone="+1 6784 345"
+          mail="Raul@mail.com"
+        />
+        <Contact
+          img="../public/dog4.jpg"
+          name="Sanchez"
+          phone="+233 6784 345"
+          mail="Sanch@mail.com"
+        />
+      </div>
+      <hr/>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
---
+++ React-tut/src/components/TravelPage/Contact.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+
+const Contact = (props) => {
+   
+    
+  return (
+    <div>
+      <div className="p-5 border-2 border-blue-300 w-80 rounded-md m-4 shadow-2xl">
+        <img src={props.img} alt="dog1" className="w-72"/>
+        <h2 className="text-2xl"> {props.name}</h2>
+        <img src="/free-phone-icon.png" alt="phone" className="flex w-5"/><span> {props.phone}</span>
+        <img src="/images.png" alt="mail" className="flex w-5"/><span> {props.mail}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
